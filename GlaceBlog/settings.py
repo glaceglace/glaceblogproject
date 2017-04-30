48d9322c6e172aa6ee7e907aa5823cf85c25169c
stylerope <spaceruler@hotmail.com>
2017-05-01 04:10:09 +0800

    added static root

--- GlaceBlog/settings.py
+++ GlaceBlog/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'm5xe3zyea0-i5#9w*&25ms$l2zo$441k+otgo3(jvdnbf+h3kq'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost', '.glaceglace.com']
 
 
 # Application definition
@@ -132,3 +132,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join (BASE_DIR, 'static')
